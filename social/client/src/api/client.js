115a7f5728143a47765cf80ee8a08fd66852b586
SreeKrishna01 <iiyyanar478@gmail.com>
2026-08-09 00:05:16 +0530

    Update API base URL to use environment variable

--- social/client/src/api/client.js
+++ social/client/src/api/client.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
