ad304e744d4fc3a74f95d83a30334ddd085bdcad
SreeKrishna01 <iiyyanar478@gmail.com>
2026-08-09 23:43:59 +0530

    Adjust padding in ms-composer style

--- social/client/src/pages/MessagesPage.jsx
+++ social/client/src/pages/MessagesPage.jsx
@@ -1142,7 +1142,7 @@ export default function MessagesPage() {
   display: flex;
   align-items: center;
   gap: 12px;
-  padding: 18px 30px 22px 14px;
+  padding: 18px 60px 50px 14px;
   border-top: 1px solid ${C.line};
 }
         .ms-composer-input-wrap {
