4eb76eb02a572d476db9162417e0db6e21524ec9
SreeKrishna01 <iiyyanar478@gmail.com>
2026-08-09 12:56:58 +0530

    Change PWA icon sources in vite.config.js
    Updated PWA icons to use 'kek-start.png'.

--- social/client/vite.config.js
+++ social/client/vite.config.js
@@ -37,12 +37,12 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/kek-start.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/kek-start.png',
             sizes: '512x512',
             type: 'image/png',
           },
