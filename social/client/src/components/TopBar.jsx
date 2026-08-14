442df11d285d157a963c7b6f8a1533e8db4a2526
Sree Krishnan <iiyyanar478@gmail.com>
2026-08-14 13:40:04 +0530

    Add border to TopBar component

--- social/client/src/components/TopBar.jsx
+++ social/client/src/components/TopBar.jsx
@@ -74,6 +74,7 @@ export default function TopBar() {
           background: url('https://c4.wallpaperflare.com/wallpaper/391/562/489/net-background-surface-dark-wallpaper-preview.jpg');
           -webkit-background-clip: text;
           color: transparent;
+          border: 1px ridge #a5a2ba;
           animation: animate 80s linear infinite;
         }
         @keyframes animate {
