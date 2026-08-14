18f4ebf09bd0b36b151ca2e1a3898b433ea18759
Sree Krishnan <iiyyanar478@gmail.com>
2026-08-14 13:49:20 +0530

    Reduce text stroke width in TopBar component

--- social/client/src/components/TopBar.jsx
+++ social/client/src/components/TopBar.jsx
@@ -74,7 +74,7 @@ export default function TopBar() {
           background: url('https://c4.wallpaperflare.com/wallpaper/391/562/489/net-background-surface-dark-wallpaper-preview.jpg');
           -webkit-background-clip: text;
           color: transparent;
-          -webkit-text-stroke: 1px #d9d3de;
+          -webkit-text-stroke: 0.5px #d9d3de;
           animation: animate 80s linear infinite;
         }
         @keyframes animate {
